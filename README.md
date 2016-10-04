2fadc8db8ebbd3dbec54b47ec6c53440da678306
Eric Sowell <eric.sowell@gmail.com>
2016-10-04 12:10:01 -0500

    progress for Omar

---
+++ README.md
@@ -0,0 +1,15 @@
+
+
+
+https://www.npmjs.com/package/mongodb
+
+
+
+
+Run this from the bin directory
+
+```
+./mongod --dbpath=../../data --port 27017
+```
+
+Of course the directory path (specified in `--dbpath`) could be different for you.
--- server.js
+++ server.js
@@ -1,10 +1,136 @@
-
 var express = require('express');
+var MongoClient = require('mongodb').MongoClient;
+var bodyParser = require('body-parser');
+
 var app = express();
 
+app.use(express.static('public'));
+
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({extended: true}));
+
+
+
+
+var mongoConnectionUrl = 'mongodb://localhost:27017/myproject';
+
+
+
+// ==================================================
+// GET ALL
+// ==================================================
+
 app.get('/api/stuff', function(req, res) {
-  res.send('hi');
+
+  MongoClient.connect(mongoConnectionUrl, function(err, db) {
+    //assert.equal(null, err);
+    console.log("Connected correctly to server");
+
+    var callbackForFindComplete = function(docs) {
+      db.close();
+      res.send(docs);
+    };
+
+    findDocuments({}, db, callbackForFindComplete);
+
+  });
+
+
 });
+
+
+
+
+
+// ==================================================
+// GET
+// ==================================================
+
+app.get('/api/stuff/:id', function(req, res) {
+
+  //step 1: get user id from parameters
+  var userId = req.params.id;
+
+  //step 2: connect to mongo
+  MongoClient.connect(mongoConnectionUrl, function(err, db) {
+    //step 3: mongo connection successful!
+    console.log("Connected correctly to server");
+
+    //step 4: now build parameters for the search
+    var searchParameters = {
+      userId: userId
+    };
+
+    //step 5: execute search
+    findDocuments(searchParameters, db, function(docs) {
+      //step 8: data is in docs, so we no longer need to keep the db connection open
+      db.close();
+      //step 9: send stats back from api. It is at this point that your done function on the client side will get its data.
+      res.send(docs);
+    });
+
+  });
+
+
+});
+
+var findDocuments = function(searchParameters, db, callback) {
+  var collection = db.collection('documents');
+  // Step 6: query the database
+  collection.find(searchParameters).toArray(function(err, docs) {
+    //step 7: callback is fired, data is in the "docs" parameter
+    console.log("Found the following records");
+    console.dir(docs);
+    callback(docs);
+  });
+}
+
+
+// ==================================================
+// POST
+// ==================================================
+
+
+app.post('/api/stuff/:id', function(req, res) {
+
+  var userId = req.params.id;
+
+  console.log('what did I get?', userId, req.body);
+
+  var dataToInsert = {
+    wins: Number(req.body.wins),
+    losses: Number(req.body.losses),
+    difficultyChosen: req.body.difficultyChosen,
+    userId: userId
+  }
+
+  // Use connect method to connect to the Server
+  MongoClient.connect(mongoConnectionUrl, function(err, db) {
+    //assert.equal(null, err);
+    console.log("Connected correctly to server");
+
+    insertDocuments(dataToInsert, db, function() {
+      db.close();
+      res.send('hello');
+    });
+
+  });
+});
+
+var insertDocuments = function(dataToInsert, db, callback) {
+  // Get the documents collection
+  var collection = db.collection('documents');
+
+  // Insert some documents
+  collection.insert(dataToInsert, function(err, result) {
+    console.log("Added something");
+    callback(result);
+  });
+}
+
+
+
+
 
 app.listen(3000, function() {
   console.log('listening on port 3000');
